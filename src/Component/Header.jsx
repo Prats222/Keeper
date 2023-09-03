38a221064027a69d984d37bf71747f88086460c7
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-09-03 21:38:02 +0530

    Initial commit

---
+++ src/Component/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Timer10RoundedIcon from '@mui/icons-material/Timer10Rounded';
+function Header() {
+  return (
+    <header>
+      <h1> Keeper <Timer10RoundedIcon /></h1>
+    </header>
+  );
+}
+
+export default Header;
